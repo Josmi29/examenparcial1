b48dabb22450de8f7aee2fffc539cc2b00c8bb5e
Josmi29 <141948673+Josmi29@users.noreply.github.com>
2023-09-07 09:24:03 -0600

    Add files via upload

---
+++ suma de digitos.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//programa que suma los digitos de un numero
+//jose miguel ruiz marquez 180200
+int main() {
+    int numero, suma = 0;
+
+    printf("escribe un numero:\n\a ");
+    scanf("%d", &numero);
+
+    printf("los digitos que se suman son:\a ");
+
+    
+    while (numero != 0) 
+	{
+        int digito = numero % 10; 
+        suma += digito;           
+        printf("%d ", digito);    
+        numero /= 10;             
+    }
+
+    printf("\nla suma de digitos es: %d\n", suma);
+    return 0;
+}
